e1746bdd3bd64b71699db12a0ad50f56e19dd7f4
David Huerta <david@haystackproject.com>
2014-05-04 20:57:20 -0400

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 emma-ohnoes
 ===========
-
 Emma O, judge of hell judges your cloud infrastructure.
+
+Requirements
+------------
+A http://www.temboo.com account and a TembooAccount.h file to store your Temboo creds
+Access to a 3D printer with:
+* Transparent blue-ish PLA plastic
+* Opaque black PLA plastic
+An Arduino Yún
+Two 10mmLEDs of the low-voltage variety. Blue and red were used for this project
+An ethernet connection to a DHCP-enabled network and ethernet cable
+A USB wall adapter and micro-usb cable
+
+Setup
+-----
+In the emma_ohnoes.ino file, change the targetUrl variable to the site you want to check the availability for, or a custom AWS health check page to check on specific infrastructure metrics (See: http://docs.aws.amazon.com/Route53/latest/DeveloperGuide/health-checks-creating-deleting.html#health-checks-creating). If the URL returns any other HTTP status code except 200 OK, Emma O will start pulsing a furious red, otherwise he'll glow a serene blue.
+
+Usage
+-----
+Plug it in, give it a few minutes until Emma turns blue, then wait for US-EAST-1 to start failwhaling to see Emma turn red.
+
+License
+-------
+This page is distributed under the MIT license. Please see the included copy of the license for deets.
---
+++ emma_ohnoes.ino
@@ -0,0 +1,75 @@
+#include <Bridge.h>
+#include <Temboo.h>
+#include "TembooAccount.h" // Contains Temboo account info, you'll need your own
+
+int numRuns = 1;    // Execution count, so this doesn't run forever
+int maxRuns = 10;   // Maximum number of times the Choreo should be executed
+int whiteLed = 13;
+int redLed = 9;
+int brightness = 0; // Start out LED with no brightness
+int fadeAmount = 5; // Points to fade the LED by
+
+void setup() {
+  // For debugging, wait until the serial console is connected.
+  //Serial.begin(9600);
+  //delay(4000);
+  //while(!Serial);
+  Bridge.begin();
+  pinMode(whiteLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+}
+
+void loop() {
+  TembooChoreo GetChoreo;
+  // Invoke the Temboo client
+  GetChoreo.begin();
+  // Set Temboo account credentials
+  GetChoreo.setAccountName(TEMBOO_ACCOUNT);
+  GetChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
+  GetChoreo.setAppKey(TEMBOO_APP_KEY);
+  // Custom user agent for less confusing logs
+  GetChoreo.addInput("RequestHeaders", "{\n    \"User-Agent\": \"Emma Ohnoes (Arduino Yún/Temboo)\"\n}");
+  // Replace our healthcheck.php url with your website to check if it's up
+  GetChoreo.addInput("URL", "http://www.brooklynmuseum.org/healthcheck.php");
+  // Plain GET request
+  GetChoreo.setChoreo("/Library/Utilities/HTTP/Get");
+  // Run the Choreo, save return code. 0 is ok, 223 means non-200 status from healthcheck
+  int returnCode = GetChoreo.run();
+
+  // AWSPOCALYPSE!
+  if (returnCode == 223) {
+    // switch off white light
+    digitalWrite(whiteLed, LOW);
+    pulseRedLed();
+  }
+  else { // Emma O's in his heaven. All's right in the world.
+    while(GetChoreo.available()) {
+      //char c = GetChoreo.read();
+      //Serial.print(c);
+      
+      // turn off red led in case it was on
+      analogWrite(redLed, 0);
+      // turn on white light to indicate Temboo's working at least
+      digitalWrite(whiteLed, HIGH);
+    }
+  }
+  
+  GetChoreo.close();
+  //Serial.println("Pinging healthcheck again...");
+  delay(60000); // wait a minute seconds between GET calls
+}
+
+void pulseRedLed() {
+  // turn on and pulse red light
+  while (brightness <= 255) {
+    analogWrite(redLed, brightness);
+    // change the brightness for next time through the loop:
+    brightness = brightness + fadeAmount;
+    // reverse the direction of the fading at the ends of the fade:
+    if (brightness == 0 || brightness == 255) {
+      fadeAmount = -fadeAmount ;
+    }
+    // wait for 30 milliseconds to see the dimming effect    
+    delay(30);
+  }
+}
